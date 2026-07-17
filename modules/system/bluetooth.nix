8a2d1975ef339773f1d3bd51c1fe523e9cd110d0
hkn-alp <147254890+hkn-alp@users.noreply.github.com>
2026-07-17 17:36:07 +0300

    Add bluetooth.nix

---
+++ modules/system/bluetooth.nix
@@ -0,0 +1,12 @@
+# modules/system/bluetooth.nix
+{ config, pkgs, ... }:
+
+{
+  # === 1. BLUETOOTH DAEMONS ===
+  
+  # Enable the Bluetooth hardware radio.
+  hardware.bluetooth.enable = true;
+  
+  # Powers up the default Bluetooth controller on boot.
+  hardware.bluetooth.powerOnBoot = true;
+}
