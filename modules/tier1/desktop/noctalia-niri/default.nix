931c16a0b7fd3a065430ee73d7e82aae0901200b
Hakan Alparslan <haga34@hotmail.com>
2026-09-01 17:07:32 +0300

    niri fix2

--- modules/tier1/desktop/noctalia-niri/default.nix
+++ modules/tier1/desktop/noctalia-niri/default.nix
@@ -1,12 +1,10 @@
-{ pkgs, lib, inputs, ... }: 
+{ pkgs, inputs, ... }: 
 {
   # --- Noctalia Shell ---
   programs.noctalia = {
     enable = true;
     systemd.enable = true;
     recommendedServices.enable = true;
-    
-    # Inject TOML configuration
     package = inputs.wrapper-modules.wrappers.noctalia-shell.wrap {
       inherit pkgs;
       settings = builtins.fromTOML (builtins.readFile ./noctalia-config.toml);
@@ -16,20 +14,22 @@
   # --- Noctalia Greeter ---
   services.displayManager.noctalia-greeter = {
     enable = true;
-    
-    # Arguments to add to the noctalia-greeter-session invocation 
     extraArgs = [ "session" "niri" ];
   };
 
-  # --- Niri Window Manager ---
-  programs.niri = {
-    enable = true;
-    package = inputs.wrapper-modules.wrappers.niri.wrap {
-      inherit pkgs;
-      settings = {
-        xwayland-satellite.path = lib.getExe pkgs.xwayland-satellite;
-      };
-      extraSettings = builtins.readFile ./niri-config.kdl;
-    };
-  };
+  # --- Niri Window Manager (Upstream NixOS Method) ---
+  programs.niri.enable = true;
+
+  # Route your raw KDL file into the system configuration directory
+  environment.etc."niri/config.kdl".source = ./niri-config.kdl;
+  
+  # Tell Niri exactly where to find it
+  environment.variables.NIRI_CONFIG = "/etc/niri/config.kdl";
+
+  # --- Wiki Recommended Fixes ---
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  
+  environment.systemPackages = [ 
+    pkgs.xwayland-satellite 
+  ];
 }
