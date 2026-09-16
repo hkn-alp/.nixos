62548e018d13d0797d342a885aa897e05c607a5a
Hakan Alparslan <haga34@hotmail.com>
2026-09-16 18:51:37 +0300

    chore: update system configuration

--- modules/programs/creative.nix
+++ modules/programs/creative.nix
@@ -1,21 +1,12 @@
 { pkgs, ... }: {
   environment.systemPackages = with pkgs; [
-    # Blender
     (pkgs.gpuWrap blender)
-
-    # Video Editor
-    (pkgs.gpuWrap davinci-resolve)
-
-    # Image Creation & Manipulation
+    # (pkgs.gpuWrap davinci-resolve)
     lorien
     pinta
     gimp
     inkscape
-
-    # Sound
     audacity
-
-    # Recording & Streaming
     obs-studio
   ];
 }
