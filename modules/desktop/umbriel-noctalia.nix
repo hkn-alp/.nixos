c51ef26f8cf39bd1c374cc503f2d5ec3dee2dd99
Hakan Alparslan <haga34@hotmail.com>
2026-09-16 18:19:05 +0300

    chore: update system configuration

--- modules/desktop/umbriel-noctalia.nix
+++ modules/desktop/umbriel-noctalia.nix
@@ -20,6 +20,23 @@
     extraArgs = [ "session" "umbriel" ];
   };
 
+  security.polkit = {
+    enable = true;
+    extraConfig = ''
+      polkit.addRule(function(action, subject) {
+        var allowedUsers = ["hakanalp"];
+
+        if (action.id == "org.noctalia.greeter.sync-appearance" &&
+            action.lookup("program") == "${pkgs.noctalia-greeter}/bin/noctalia-greeter-apply-appearance" &&
+            action.lookup("user") == "root" &&
+            subject.local && subject.active &&
+            allowedUsers.indexOf(subject.user) >= 0) {
+          return polkit.Result.YES;
+        }
+      });
+    '';
+  };
+
   # --- Umbriel Window Manager ---
   programs.umbriel.enable = true;
 
