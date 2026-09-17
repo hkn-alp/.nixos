fd840614fac4abcd4ca77a7377f0a0fae0fd1b78
Hakan Alparslan <haga34@hotmail.com>
2026-09-17 14:05:47 +0300

    chore: update system configuration

--- modules/programs/creative.nix
+++ modules/programs/creative.nix
@@ -2,6 +2,7 @@
   environment.systemPackages = with pkgs; [
     (pkgs.gpuWrap blender) # Blender with GPU wrapper
     # (pkgs.gpuWrap davinci-resolve) # DaVinci Resolve with GPU wrapper
+    shotcut # Video Editor
     lorien # Infinite Canvas Note Taking App
     pinta # Painting Made Simple
     gimp # GNU Image Manipulation Program
