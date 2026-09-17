2fb685cfa807d059dafcfbe8e40fe4ae753b01b2
Hakan Alparslan <haga34@hotmail.com>
2026-09-18 01:47:42 +0300

    chore: update system configuration

--- modules/core/maintenance.nix
+++ modules/core/maintenance.nix
@@ -1,7 +1,11 @@
 { config, pkgs, ... }: {
   # === 1. DRIVE MEMORY SAVER (GARBAGE COLLECTION) ===
-  nix.settings.auto-optimise-store = true;
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings = {
+      auto-optimise-store = true;
+      experimental-features = [ "nix-command" "flakes" ];
+      trusted-users = [ "root" "@wheel" ];
+    };
+
   nix.gc = {
     automatic = true;
     dates = "weekly";
