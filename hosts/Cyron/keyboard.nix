cb041f7a89a749698a68ad571d9e3c4a6b172918
Hakan Alparslan <haga34@hotmail.com>
2026-09-18 01:02:08 +0300

    chore: update system configuration

--- hosts/Cyron/keyboard.nix
+++ hosts/Cyron/keyboard.nix
@@ -1,7 +1,12 @@
-{ ... }: {
+{ pkgs, ... }: {
 
-  services.udev.extraHwdb = ''
-    evdev:atkbd:dmi:bvn*:bvr*:bd*:svnCASPER*:pnEXCALIBUR*:*
-     KEYBOARD_KEY_e011=f13
-  '';
+  # Map Casper Excalibur Wi-Fi key (e011) to F13 (183) on boot
+  systemd.services.map-wifi-key = {
+    description = "Map custom Wi-Fi key to F13";
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.kbd}/bin/setkeycodes e011 183";
+    };
+  };
 }
